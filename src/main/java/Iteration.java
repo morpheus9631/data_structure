21d749a741df1e01a181bf568014d5b6235f50bf
morpheus9631 <morpheus.hsieh@gmail.com>
2024-04-11 22:57:35 +0800

    Add Itertion and Recusive

---
+++ src/main/java/Iteration.java
@@ -0,0 +1,20 @@
+package main.java;
+
+
+public class Iteration {
+
+    public static void main(String[] args) {
+        
+        Iteration iteration = new Iteration();
+        int sum = iteration.sum(10);
+        System.out.println("\n1 +...+ 10 = " + sum);
+    }
+
+    public int sum(int n) {
+        int sum = 0;
+        for (int i=1; i<=n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
